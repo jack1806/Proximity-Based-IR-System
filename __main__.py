cba1637ec3e4fe1157596a482606e5e2e8386fef
jack1806 <root@localhost.localdomain>
2018-09-09 00:20:53 +0530

    bugs fixed

--- __main__.py
+++ __main__.py
@@ -1,5 +1,6 @@
 import csv
-from autocorrect import spell
+import nltk
+# from autocorrect import spell
 from DocumentSearch import DocumentSearch
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
@@ -9,11 +10,22 @@ DATA_STORE_LOCATION = "dataset"
 WORDS_DATA_LOCATION = "words_data"
 
 
+def get_prior(text):
+    lst = ['NN', 'NNS', 'NNP', 'NNPS', 'FW', 'JJ', 'JJR', 'JJS', 'RB', 'RBR', 'RBS', 'VB', 'VBD', 'VBG', 'VBP', 'VBN', 'VBP', 'VBZ', 'PRP', 'DT', 'IN', 'CD', 'EX', 'LS', 'MD', 'PDT', 'POS', 'PRP$', 'RP', 'SYM', 'TO', 'UH', 'WDT', 'WP', 'WP$', 'WRB']
+    lst.reverse()
+    tagged = nltk.pos_tag(text)
+    list1 = []
+    for gp_i in tagged:
+        gp_j = (lst.index(gp_i[1])+1)/len(lst)
+        list1.append(str(gp_j))
+    return list1
+
+
 def proxy_dist(a, b):
     return float(1)/(abs(a**2-b**2))
 
 
-def main_search(m_words, m_data, w_count):
+def main_search(m_words, m_data, w_count, prior):
     m_score = float(0)
     if len(words) > 1:
         for m_i in range(len(m_words)//2):
@@ -27,9 +39,9 @@ def main_search(m_words, m_data, w_count):
                         m_score += proxy_dist(m_a, m_b)
             else:
                 if m_words[m_i] in m_data[0]:
-                    m_score += float(m_data[m_data[0].index(m_words[m_i])+1][1])/w_count
+                    m_score += float(m_data[m_data[0].index(m_words[m_i])+1][1])*float(prior[m_i])/w_count
                 if m_words[m_i+1] in m_data[0]:
-                    m_score += float(m_data[m_data[0].index(m_words[m_i+1])+1][1])/w_count
+                    m_score += float(m_data[m_data[0].index(m_words[m_i+1])+1][1])*float(prior[m_i+1])/w_count
     elif words[0] in m_data[0]:
         m_score += float(m_data[m_data[0].index(words[0])+1][1])/w_count
     return m_score
@@ -37,44 +49,54 @@ def main_search(m_words, m_data, w_count):
 
 if __name__ == "__main__":
     query = input("Query : ")
-    f_query = ""
-    for i in query.split():
-        f_query += spell(i)+" "
-    query = f_query
+    # f_query = ""
+    # for i in query.split():
+    #     f_query += spell(i)+" "
+    # query = f_query
+
+    print(query)
+
     punctuations = ['(', ')', ';', ':', '[', ']', ',', '.', "'s", '-']
     stop_words = stopwords.words('english')
     tokens = word_tokenize(query, 'english')
     words = [word.lower() for word in tokens if not word.lower() in stop_words and not word.lower() in punctuations]
-    print("\n".join(words))
 
-    doc = DocumentSearch()
-    csvFiles = doc.search("csv")
-    dic = {}
+    word_weights = get_prior(words)
 
-    with open(WORDS_DATA_LOCATION+"/total", 'r') as w:
-        words_count = int(w.readline())
+    if words:
+        print(" ".join(words))
+        print(" ".join(word_weights))
 
-    for csvData in csvFiles:
-        with open(csvData) as f:
-            reader = csv.reader(f)
-            data = [list(d) for d in reader]
-            # print(data)
-            score = main_search(words, data, words_count)
-            if score in dic:
-                dic[score].append(csvData)
-            else:
-                dic[score] = [csvData]
+        doc = DocumentSearch()
+        csvFiles = doc.search("csv")
+        dic = {}
 
-    rank = 1
-    for i in sorted(dic.keys(), reverse=True):
-        for j in dic[i]:
-            if rank > 5:
-                if input("Next 5 ?") == "Y":
-                    rank = 1
-                    continue
+        with open(WORDS_DATA_LOCATION+"/total", 'r') as w:
+            words_count = int(w.readline())
+
+        for csvData in csvFiles:
+            with open(csvData) as f:
+                reader = csv.reader(f)
+                data = [list(d) for d in reader]
+                # print(data)
+                score = main_search(words, data, words_count, word_weights)
+                if score in dic:
+                    dic[score].append(csvData)
                 else:
-                    break
-            with open(j.replace(WORDS_DATA_LOCATION, TEXT_STORE_LOCATION).replace(".csv", "")) as final:
-                loc = final.readline()
-            print(rank, "->", loc, i)
-            rank += 1
+                    dic[score] = [csvData]
+
+        rank = 1
+        for i in sorted(dic.keys(), reverse=True):
+            for j in dic[i]:
+                if rank > 5:
+                    if input("Next 5 ?") == "Y":
+                        rank = 1
+                        continue
+                    else:
+                        break
+                with open(j.replace(WORDS_DATA_LOCATION, TEXT_STORE_LOCATION).replace(".csv", "")) as final:
+                    loc = final.readline()
+                print(rank, "->", loc.split("/")[-1], i)
+                rank += 1
+    else:
+        print("Error")
